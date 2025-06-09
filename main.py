f15c64f5df0d7ed0cb7f20a850f2f08440ea8526
Gabriel Sá <gabriel10043@live.com>
2025-06-08 23:23:22 -0300

    update processing output

--- main.py
+++ main.py
@@ -3,29 +3,32 @@
 import sys
 
 def header(linha):
-	if linha.find("#") == 0:
-		print(f"\033[31m{linha}\033[0m", end= "")
-		return True
-	return False
-		
+    if linha.find("#") == 0:
+        linha = "\033[31m" + linha + "\033[0m" 
+    return linha	
+
+
 def bold(linha):
-	if linha.find("**") == -1:
-		return False
-	sections = linha.split("**")
-	sections_len = len(sections)
-	for i in range(0, sections_len, 2):
-		print(sections[i], end="")
-		if i+1 < sections_len:
-			print("\033[1m" + sections[i+1] + "\033[0m", end="")
-	return True
+    if linha.find("**") == -1:
+        return linha
+    sections = linha.split("**")
+    linha = ""
+    for i in range(0, len(sections), 2):
+        linha += sections[i]
+        if i+1 < len(sections):
+            linha += "\033[1m" + sections[i+1] + "\033[0m"
+    return linha
 
 if len(sys.argv) < 2:
     print(f"Usage: {sys.argv[0]} FILENAME")
     exit()
 
-arquivo = open(sys.argv[1], "r")
-
-linhas = arquivo.readlines()
-for linha in linhas:
-	if not header(linha) and not bold(linha):
-		print(linha, end= "")
+sys.argv.pop(0)
+for filename in sys.argv:
+    arquivo = open(filename, "r")
+    linhas = arquivo.readlines()
+    arquivo.close()
+    for linha in linhas:
+        linha = header(linha)
+        linha = bold(linha)
+        print(linha, end= "")
